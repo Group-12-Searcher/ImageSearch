0f7bc04af75bc2a6c9287cb1d25482a442c0eac9
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2016-09-08 19:12:38 +0800

    Added flushing of old query+result display

--- UI_search.py
+++ UI_search.py
@@ -7,11 +7,18 @@ import tkFileDialog
 from PIL import Image, ImageTk
 
 class UI_class:
+    
     def __init__(self, master, search_path):
         self.search_path = search_path
         self.master = master
         topframe = Frame(self.master)
         topframe.pack()
+        # these variables are declared for implementing the flushing of old (query + results)
+        self.isUploadingImage = False
+        self.result_img_frame = Frame(self.master)
+        self.result_img_frame.pack()
+        self.query_img_frame = Frame(self.master)
+        self.query_img_frame.pack()
 
         #Buttons
         topspace = Label(topframe).grid(row=0, columnspan=2)
@@ -25,7 +32,11 @@ class UI_class:
 
 
     def browse_query_img(self):
-
+        # user wants to enter a query; flush old (query + results)
+        self.isUploadingImage = True
+        self.query_img_frame.destroy()
+        self.result_img_frame.pack_forget()
+        # add new frame for showing new query image
         self.query_img_frame = Frame(self.master)
         self.query_img_frame.pack()
         from tkFileDialog import askopenfilename
@@ -49,9 +60,11 @@ class UI_class:
 
 
     def show_results_imgs(self):
-        self.result_img_frame = Frame(self.master)
+        # user now wants to commence the search; load frame for displaying results of query
+        self.isUploadingImage = False;
+        # self.result_img_frame = Frame(self.master)
         self.result_img_frame.pack()
-
+        
         # perform the search
         searcher = Searcher("index.csv")
         results = searcher.search(self.queryfeatures)
@@ -63,7 +76,8 @@ class UI_class:
             # load the result image and display it
             image_count += 1
             r, c = divmod(image_count - 1, COLUMNS)
-            im = Image.open( self.search_path + "/" + resultID)
+            # print(resultID)
+            im = Image.open(self.search_path + "/" + resultID)
             resized = im.resize((100, 100), Image.ANTIALIAS)
             tkimage = ImageTk.PhotoImage(resized)
             myvar = Label(self.result_img_frame, image=tkimage)
@@ -74,5 +88,4 @@ class UI_class:
 
 
 root = Tk()
-print(root)
 window = UI_class(root,'dataset')
