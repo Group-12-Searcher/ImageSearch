b8720e9747e83988015d7921499347d2691c166b
xaterz <ruofan.yu@gmail.com>
2016-09-08 18:34:00 +0800

    Added semanticsreader.py

---
+++ pyimagesearch/semanticsreader.py
@@ -0,0 +1,16 @@
+import csv
+
+class SemanticsReader:
+        def __init__(self):
+            self
+
+        def read(self, file):
+            reader = csv.reader(open(file), delimiter=" ")
+            data = []
+            for line in reader: #the text file should only have 1 line
+                for value in line:
+                    data.append(value)
+
+            return data
+
+
