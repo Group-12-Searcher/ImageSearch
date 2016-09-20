e93615ae5d22d02ea076b835807319a06ec0a0a2
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2016-09-20 17:50:32 +0800

    Implemented variable weights for features

--- UI_search.py
+++ UI_search.py
@@ -6,34 +6,276 @@ from sift.pgmconverter import convertQueryToGray
 import cv2
 from Tkinter import *
 import tkFileDialog
+import tkMessageBox
 from PIL import Image, ImageTk
 import os
 import subprocess
 import csv
 
 class UI_class:
-    def __init__(self, master, search_path):
+    
+    def __init__(self, master, search_path):        
         self.search_path = search_path
         self.master = master
         topframe = Frame(self.master)
-        topframe.pack()
         # these variables are declared for implementing the flushing of old (query + results)
         self.isUploadingImage = False
+
+        #Buttons
+        self.colorHist = IntVar()
+        self.visualKeyword = IntVar()
+        self.visualConcept = IntVar()
+        self.deepLearning = IntVar()
+        self.textFeature = IntVar()
+        self.option_vars = [self.colorHist, self.visualKeyword, self.visualConcept, self.deepLearning, self.textFeature]
+        self.option_flags = [0,0,0,0,0]
+        self.option_weights = [0,0,0,0,0]
+        self.option_boxes = []
+        self.option_sliders = []
+        self.count = 0
+
+        # Color Histogram inputs
+        self.ch = Label(topframe).grid(row=1, columnspan=2)
+        self.checkButton1 = Checkbutton(master, text="Color Histogram", variable=self.colorHist)
+        self.checkButton1.deselect()
+        self.option_boxes.append(self.checkButton1)
+        self.option_boxes[0].bind("<Button-1>", self.toggle_ch_slider)
+        self.ch_scale = Scale(master, from_=0, to=1, resolution=0.01, orient=HORIZONTAL)
+        self.ch_scale.config(state=DISABLED, length=800)
+        self.ch_scale.bind("<Button-1>", self.register_ch_original_value)
+        self.ch_scale.bind("<ButtonRelease-1>", self.ch_adjust_others)
+        self.option_sliders.append(self.ch_scale)
+        self.checkButton1.pack()
+        self.ch_scale.pack()
+
+        # Visual Keywords inputs
+        self.vk = Label(topframe).grid(row=1, columnspan=2)
+        self.checkButton2 = Checkbutton(master, text="Visual Keyword", variable=self.visualKeyword)
+        self.checkButton2.deselect()
+        self.option_boxes.append(self.checkButton2)
+        self.option_boxes[1].bind("<Button-1>", self.toggle_vk_slider)
+        self.checkButton2.pack()
+        self.vk_scale = Scale(master, from_=0, to=1, resolution=0.01, orient=HORIZONTAL)
+        self.vk_scale.config(state=DISABLED, length=800)
+        self.vk_scale.bind("<Button-1>", self.register_vk_original_value)
+        self.vk_scale.bind("<ButtonRelease-1>", self.vk_adjust_others)
+        self.option_sliders.append(self.vk_scale)
+        self.vk_scale.pack()
+
+        # Visual Concepts inputs
+        self.vc = Label(topframe).grid(row=1, columnspan=2)
+        self.checkButton3 = Checkbutton(master, text="Visual Concept", variable=self.visualConcept)
+        self.checkButton3.deselect()
+        self.option_boxes.append(self.checkButton3)
+        self.option_boxes[2].bind("<Button-1>", self.toggle_vc_slider)
+        self.checkButton3.pack()
+        self.vc_scale = Scale(master, from_=0, to=1, resolution=0.01, orient=HORIZONTAL)
+        self.vc_scale.config(state=DISABLED, length=800)
+        self.vc_scale.bind("<Button-1>", self.register_vc_original_value)
+        self.vc_scale.bind("<ButtonRelease-1>", self.vc_adjust_others)
+        self.option_sliders.append(self.vc_scale)
+        self.vc_scale.pack()
+
+        # Deep Learning inputs
+        self.dl = Label(topframe).grid(row=1, columnspan=2)
+        self.checkButton4 = Checkbutton(master, text="Deep Learning", variable=self.deepLearning)
+        self.checkButton4.deselect()
+        self.option_boxes.append(self.checkButton4)
+        self.option_boxes[3].bind("<Button-1>", self.toggle_dl_slider)
+        self.checkButton4.pack()
+        self.dl_scale = Scale(master, from_=0, to=1, resolution=0.01, orient=HORIZONTAL)
+        self.dl_scale.config(state=DISABLED, length=800)
+        self.dl_scale.bind("<Button-1>", self.register_dl_original_value)
+        self.dl_scale.bind("<ButtonRelease-1>", self.dl_adjust_others)
+        self.option_sliders.append(self.dl_scale)
+        self.dl_scale.pack()
+
+        # Text Features inputs
+        self.tf = Label(topframe).grid(row=1, columnspan=2)
+        self.checkButton5 = Checkbutton(master, text="Text Feature", variable=self.textFeature)
+        self.checkButton5.deselect()
+        self.option_boxes.append(self.checkButton5)
+        self.option_boxes[4].bind("<Button-1>", self.toggle_tf_slider)
+        self.checkButton5.pack()
+        self.tf_scale = Scale(master, from_=0, to=1, resolution=0.01, orient=HORIZONTAL)
+        self.tf_scale.config(state=DISABLED, length=800)
+        self.tf_scale.bind("<Button-1>", self.register_tf_original_value)
+        self.tf_scale.bind("<ButtonRelease-1>", self.tf_adjust_others)
+        self.option_sliders.append(self.tf_scale)
+        self.tf_scale.pack()
+        
+        self.bbutton= Button(topframe, text=" Choose an image ", command=self.browse_query_img)
+        self.bbutton.grid(row=1, column=1)
+        self.cbutton = Button(topframe, text=" Search ", command=self.show_results_imgs)
+        self.cbutton.grid(row=1, column=2)
+
         self.result_img_frame = Frame(self.master)
         self.result_img_frame.pack()
         self.query_img_frame = Frame(self.master)
         self.query_img_frame.pack()
 
-        #Buttons
-        topspace = Label(topframe).grid(row=0, columnspan=2)
-        self.bbutton= Button(topframe, text=" Choose an image ", command=self.browse_query_img)
-        self.bbutton.grid(row=1, column=1)
-        self.cbutton = Button(topframe, text=" Search ", command=self.show_results_imgs)
-        self.cbutton.grid(row=1, column=2)
-        downspace = Label(topframe).grid(row=3, columnspan=4)
+        downspace = Label(topframe).grid(row=7, columnspan=1)
 
+        topframe.pack()
+    
         self.master.mainloop()
 
+    def toggle_ch_slider(self, event):
+        if self.option_vars[0].get() == 0:
+            self.count = self.count + 1
+            self.option_flags[0] = 1
+            self.option_sliders[0].config(state=NORMAL)
+            for i in range(0, 5):
+                if self.option_flags[i] == 1:
+                    self.option_sliders[i].set(1.0 / self.count)
+        else:
+            self.count = self.count - 1
+            self.option_flags[0] = 0
+            self.option_sliders[0].set(0.0)
+            self.option_sliders[0].config(state=DISABLED)
+            if self.count != 0:
+                for i in range(0, 5):
+                    if self.option_flags[i] == 1:
+                        self.option_sliders[i].set(1.0 / self.count)
+
+    def toggle_vk_slider(self, event):
+        if self.option_vars[1].get() == 0:
+            self.count = self.count + 1
+            self.option_flags[1] = 1
+            self.option_sliders[1].config(state=NORMAL)
+            for i in range(0, 5):
+                if self.option_flags[i] == 1:
+                    self.option_sliders[i].set(1.0 / self.count)
+        else:
+            self.count = self.count - 1
+            self.option_flags[1] = 0
+            self.option_sliders[1].set(0.0)
+            self.option_sliders[1].config(state=DISABLED)
+            if self.count != 0:
+                for i in range(0, 5):
+                    if self.option_flags[i] == 1:
+                        self.option_sliders[i].set(1.0 / self.count)
+
+    def toggle_vc_slider(self, event):
+        if self.option_vars[2].get() == 0:
+            self.count = self.count + 1
+            self.option_flags[2] = 1
+            self.option_sliders[2].config(state=NORMAL)
+            for i in range(0, 5):
+                if self.option_flags[i] == 1:
+                    self.option_sliders[i].set(1.0 / self.count)
+        else:
+            self.count = self.count - 1
+            self.option_flags[2] = 0
+            self.option_sliders[2].set(0.0)
+            self.option_sliders[2].config(state=DISABLED)
+            if self.count != 0:
+                for i in range(0, 5):
+                    if self.option_flags[i] == 1:
+                        self.option_sliders[i].set(1.0 / self.count)
+
+    def toggle_dl_slider(self, event):
+        if self.option_vars[3].get() == 0:
+            self.count = self.count + 1
+            self.option_flags[3] = 1
+            self.option_sliders[3].config(state=NORMAL)
+            for i in range(0, 5):
+                if self.option_flags[i] == 1:
+                    self.option_sliders[i].set(1.0 / self.count)
+        else:
+            self.count = self.count - 1
+            self.option_flags[3] = 0
+            self.option_sliders[3].set(0.0)
+            self.option_sliders[3].config(state=DISABLED)
+            if self.count != 0:
+                for i in range(0, 5):
+                    if self.option_flags[i] == 1:
+                        self.option_sliders[i].set(1.0 / self.count)
+
+    def toggle_tf_slider(self, event):
+        if self.option_vars[4].get() == 0:
+            self.count = self.count + 1
+            self.option_flags[4] = 1
+            self.option_sliders[4].config(state=NORMAL)
+            for i in range(0, 5):
+                if self.option_flags[i] == 1:
+                    self.option_sliders[i].set(1.0 / self.count)
+        else:
+            self.count = self.count - 1
+            self.option_flags[4] = 0
+            self.option_sliders[4].set(0.0)
+            self.option_sliders[4].config(state=DISABLED)
+            if self.count != 0:
+                for i in range(0, 5):
+                    if self.option_flags[i] == 1:
+                        self.option_sliders[i].set(1.0 / self.count)
+
+    def register_ch_original_value(self, event):
+        self.option_weights[0] = self.option_sliders[0].get()
+        #print("CH: %s" % (self.option_weights[0]))
+
+    def register_vk_original_value(self, event):
+        self.option_weights[1] = self.option_sliders[1].get()
+        #print("VK: %s" % (self.option_weights[1]))
+
+    def register_vc_original_value(self, event):
+        self.option_weights[2] = self.option_sliders[2].get()
+        #print("VC: %s" % (self.option_weights[2]))
+
+    def register_dl_original_value(self, event):
+        self.option_weights[3] = self.option_sliders[3].get()
+        #print("DL: %s" % (self.option_weights[3]))
+
+    def register_tf_original_value(self, event):
+        self.option_weights[4] = self.option_sliders[4].get()
+        #print("TF: %s" % (self.option_weights[4]))
+
+    def ch_adjust_others(self, value):
+        if self.count > 1:
+            difference = (self.option_weights[0] - self.option_sliders[0].get()) / (self.count - 1)
+            self.option_weights[0] = self.option_sliders[0].get()
+            print(difference)
+            for i in range(0, 5):
+                if i != 0 and self.option_flags[i] == 1:
+                    self.option_weights[i] = self.option_weights[i] + difference
+                    self.option_sliders[i].set(self.option_weights[i])
+
+    def vk_adjust_others(self, event):
+        if self.count > 1:
+            difference = (self.option_weights[1] - self.option_sliders[1].get()) / (self.count - 1)
+            self.option_weights[1] = self.option_sliders[1].get()
+            print(difference)
+            for i in range(0, 5):
+                if i != 1 and self.option_flags[i] == 1:
+                    self.option_weights[i] = self.option_weights[i] + difference
+                    self.option_sliders[i].set(self.option_weights[i])
+
+    def vc_adjust_others(self, event):
+        if self.count > 1:
+            difference = (self.option_weights[2] - self.option_sliders[2].get()) / (self.count - 1)
+            self.option_weights[2] = self.option_sliders[2].get()
+            print(difference)
+            for i in range(0, 5):
+                if i != 2 and self.option_flags[i] == 1:
+                    self.option_sliders[i].set(self.option_sliders[i].get() + difference)
+
+    def dl_adjust_others(self, event):
+        if self.count > 1:
+            difference = (self.option_weights[3] - self.option_sliders[3].get()) / (self.count - 1)
+            self.option_weights[3] = self.option_sliders[3].get()
+            print(difference)
+            for i in range(0, 5):
+                if i != 3 and self.option_flags[i] == 1:
+                    self.option_sliders[i].set(self.option_sliders[i].get() + difference)
+
+    def tf_adjust_others(self, event):
+        if self.count > 1:
+            difference = (self.option_weights[4] - self.option_sliders[4].get()) / (self.count - 1)
+            self.option_weights[4] = self.option_sliders[4].get()
+            print(difference)
+            for i in range(0, 5):
+                if i != 4 and self.option_flags[i] == 1:
+                    self.option_sliders[i].set(self.option_sliders[i].get() + difference)
 
     def browse_query_img(self):
         # user wants to enter a query; flush old (query + results)
@@ -97,15 +339,30 @@ class UI_class:
 
         self.query_img_frame.mainloop()
 
+    def get_options(self):
+        options = {}
+        options['ch'] = self.option_sliders[0].get()
+        options['vk'] = self.option_sliders[1].get()
+        options['vc'] = self.option_sliders[2].get()
+        options['dl'] = self.option_sliders[3].get()
+        options['tf'] = self.option_sliders[4].get()
+        return options
 
     def show_results_imgs(self):
+        
         # user now wants to commence the search; load frame for displaying results of query
         self.isUploadingImage = False;
         # self.result_img_frame = Frame(self.master)
         self.result_img_frame.pack()
 
+        if (self.count == 0):
+            tkMessageBox.showwarning("Alert", "Please select at least 1 option.")
+            self.result_img_frame.mainloop()
+
+        flags = self.get_options()
+
         # perform the search
-        searcher = Searcher("index.csv", "index_semantics.csv", "index_text.csv")
+        searcher = Searcher("index.csv", "index_semantics.csv", "index_text.csv", flags)
         results = searcher.search(self.queryfeatures, self.querysemantics, self.querytext)
 
         # show result pictures
--- pyimagesearch/searcher.py
+++ pyimagesearch/searcher.py
@@ -9,15 +9,15 @@ import threading
 from mysift import myMatch, myMatch2
 
 class Searcher:        
-	def __init__(self, indexPath, semanticsPath, textPath):
+	def __init__(self, indexPath, semanticsPath, textPath, flags):
 		# store our index path
 		self.indexPath = indexPath
 		self.semanticsPath = semanticsPath
 		self.textPath = textPath
-                self.WEIGHT_CH = 0.2
-                self.WEIGHT_SEM = 0.5
-                self.WEIGHT_SIFT = 0.2
-                self.WEIGHT_TEXT = 0.1
+                self.WEIGHT_CH = 0.2 * flags["ch"]
+                self.WEIGHT_SEM = 0.5 * flags["vc"]
+                self.WEIGHT_SIFT = 0.2 * flags["vk"]
+                self.WEIGHT_TEXT = 0.1 * flags["tf"]
                 self.results = {}
 
         class myThread(threading.Thread):
@@ -41,110 +41,119 @@ class Searcher:
 
                 numMatches = line.split(' ')[1]
                 d = float(numMatches)
-                self.searcher.results["dataset\\dataset\\"+self.dbImage+".jpg"] -= d * self.searcher.WEIGHT_SIFT                
+                if self.searcher.results.get("dataset\\dataset\\"+self.dbImage+".jpg") is None:
+                        self.searcher.results["dataset\\dataset\\"+self.dbImage+".jpg"] = d * self.searcher.WEIGHT_SIFT
+                else:
+                        self.searcher.results["dataset\\dataset\\"+self.dbImage+".jpg"] -= d * self.searcher.WEIGHT_SIFT                
 
-	def search(self, queryFeatures, querySemantics, queryText, limit = 32):
-		# open the color histogram index file for reading
-		with open(self.indexPath) as f:
-			# initialize the CSV reader
-			reader = csv.reader(f)
+	def search(self, queryFeatures, querySemantics, queryText, limit = 16):
+                if self.WEIGHT_CH != 0:
+                        # open the color histogram index file for reading
+                        with open(self.indexPath) as f:
+                                # initialize the CSV reader
+                                reader = csv.reader(f)
 
-			# loop over the rows in the index
-			for row in reader:
-				# parse out the image ID and features, then compute the
-				# chi-squared distance between the features in our index
-				# and our query features
-				features = [float(x) for x in row[1:]]
-				d = self.chi2_distance(features, queryFeatures)
-				
-				# now that we have the distance between the two feature
-				# vectors, we can udpate the self.results dictionary -- the
-				# key is the current image ID in the index and the
-				# value is the distance we just computed, representing
-				# how 'similar' the image in the index is to our query
-				self.results[row[0]] = d * self.WEIGHT_CH
-				
-			# close the reader
-			f.close()
-
-		# open the semantics index file for reading
-		with open(self.semanticsPath) as f:
-			# initialize the CSV reader
-			reader = csv.reader(f)
-
-			# loop over the rows in the index
-			for row in reader:
-				# parse out the image ID and features, then compute the
-				# chi-squared distance between the features in our index
-				# and our query features
-				features = [float(x) for x in row[1:]]
-				'''
-                                features = []
-                                for x in row[1:]:
-                                        print(x)
-                                        print("---")
-                                        features.append(float(x))
-                                '''
-
-                                                        
-				d = self.chi2_distance(features, querySemantics)
-				
-				# now that we have the distance between the two feature
-				# vectors, we can udpate the results dictionary -- the
-				# key is the current image ID in the index and the
-				# value is the distance we just computed, representing
-				# how 'similar' the image in the index is to our query
-				self.results[row[0]] += d * self.WEIGHT_SEM
-				
-			# close the reader
-			f.close()
-
-
-                # Do sift matching
-                os.chdir("sift")
-                threads = []
-                for f in glob.glob("*.key"):
-                        dbImage = f.split('/')[-1].split('.')[0]
-                        t = self.myThread(self, dbImage)
-                        t.start()
-                        threads.append(t)
-                for t in threads:
-                        t.join()
-                        
-                ### My own experimental sift ###
-                '''
-                os.chdir("sift")
-                for f in glob.glob("*.key"):
-                        dbImage = f.split('/')[-1].split('.')[0]
-                        im1 = "temp/query.pgm"
-                        im2 = dbImage+".pgm"
-                        d = myMatch(im1, im2)
-                        self.results["dataset\\dataset\\"+dbImage+".jpg"] -= d * self.WEIGHT_SIFT
-                '''
-                
-                os.chdir("..")
-
-                # open the text index file for reading
-		with open(self.textPath) as f:
-			# initialize the CSV reader
-			reader = csv.reader(f)
-                        # Loop over all the tagged words for query image
-                        if (queryText != None):
-                                for word in queryText:
-                                        print("word is : " + word)
-                                        # loop over the rows in the index
-                                        for row in reader:
-                                                if (row[0] == word):
-                                                        # loop over all matching images
-                                                        for match_img in row[1:]:
-                                                                req_name = "dataset\\dataset\\" + match_img
-                                                                print (req_name)
-                                                                if (self.results.get(req_name) != None):
-                                                                        self.results[req_name] -= self.WEIGHT_TEXT
-                                                        break # go to next word
+                                # loop over the rows in the index
+                                for row in reader:
+                                        # parse out the image ID and features, then compute the
+                                        # chi-squared distance between the features in our index
+                                        # and our query features
+                                        features = [float(x) for x in row[1:]]
+                                        d = self.chi2_distance(features, queryFeatures)
                                         
-                        # close the reader
-                        f.close()
+                                        # now that we have the distance between the two feature
+                                        # vectors, we can udpate the self.results dictionary -- the
+                                        # key is the current image ID in the index and the
+                                        # value is the distance we just computed, representing
+                                        # how 'similar' the image in the index is to our query
+                                        self.results[row[0]] = d * self.WEIGHT_CH
+                                        
+                                # close the reader
+                                f.close()
+
+                if self.WEIGHT_SEM != 0:
+                        # open the semantics index file for reading
+                        with open(self.semanticsPath) as f:
+                                # initialize the CSV reader
+                                reader = csv.reader(f)
+
+                                # loop over the rows in the index
+                                for row in reader:
+                                        # parse out the image ID and features, then compute the
+                                        # chi-squared distance between the features in our index
+                                        # and our query features
+                                        features = [float(x) for x in row[1:]]
+                                        '''
+                                        features = []
+                                        for x in row[1:]:
+                                                print(x)
+                                                print("---")
+                                                features.append(float(x))
+                                        '''
+
+                                                                
+                                        d = self.chi2_distance(features, querySemantics)
+                                        
+                                        # now that we have the distance between the two feature
+                                        # vectors, we can udpate the results dictionary -- the
+                                        # key is the current image ID in the index and the
+                                        # value is the distance we just computed, representing
+                                        # how 'similar' the image in the index is to our query
+                                        if self.results.get(row[0]) is None:
+                                                self.results[row[0]] = d * self.WEIGHT_SEM
+                                        else:
+                                                self.results[row[0]] += d * self.WEIGHT_SEM
+                                        
+                                # close the reader
+                                f.close()
+
+                if self.WEIGHT_SIFT != 0:
+                        # Do sift matching
+                        os.chdir("sift")
+                        threads = []
+                        for f in glob.glob("*.key"):
+                                dbImage = f.split('/')[-1].split('.')[0]
+                                t = self.myThread(self, dbImage)
+                                t.start()
+                                threads.append(t)
+                        for t in threads:
+                                t.join()
+                                
+                        ### My own experimental sift ###
+                        '''
+                        os.chdir("sift")
+                        for f in glob.glob("*.key"):
+                                dbImage = f.split('/')[-1].split('.')[0]
+                                im1 = "temp/query.pgm"
+                                im2 = dbImage+".pgm"
+                                d = myMatch(im1, im2)
+                                self.results["dataset\\dataset\\"+dbImage+".jpg"] -= d * self.WEIGHT_SIFT
+                        '''
+                        
+                        os.chdir("..")
+
+                if self.WEIGHT_TEXT != 0:
+                        # open the text index file for reading
+                        with open(self.textPath) as f:
+                                # initialize the CSV reader
+                                reader = csv.reader(f)
+                                # Loop over all the tagged words for query image
+                                if (queryText != None):
+                                        for word in queryText:
+                                                print("word is : " + word)
+                                                # loop over the rows in the index
+                                                for row in reader:
+                                                        if (row[0] == word):
+                                                                # loop over all matching images
+                                                                for match_img in row[1:]:
+                                                                        req_name = "dataset\\dataset\\" + match_img
+                                                                        print (req_name)
+                                                                        if (self.results.get(req_name) != None):
+                                                                                self.results[req_name] -= self.WEIGHT_TEXT
+                                                                break # go to next word
+                                                
+                                # close the reader
+                                f.close()
 
 		# sort our results, so that the smaller distances (i.e. the
 		# more relevant images are at the front of the list)
